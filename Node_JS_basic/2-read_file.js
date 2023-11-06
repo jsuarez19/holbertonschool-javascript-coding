0c6d1f9a53a51c4fbb5402c1f1081186edd2ff80
Jose Suarez <jolusuvi@gmail.com>
2023-11-05 23:38:58 -0500

    Use a for-of bucle instead a for-in

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -12,11 +12,6 @@ function countStudents(path) {
       const columns = line.split(',');
       const [firstName, , , field] = columns.map((column) => column.trim());
 
-
-      if (!field || !firstName) {
-        continue; // Saltar líneas incompletas
-      }
-
       if (studentCountsByField[field]) {
         studentCountsByField[field] += 1;
       } else {
@@ -27,20 +22,18 @@ function countStudents(path) {
     }
 
     console.log(`Number of students: ${totalStudents}`);
-
-    for (const field in studentCountsByField) {
-      console.log(`Number of students in ${field}: ${studentCountsByField[field]}. List: ${lines
+    
+    for (const field of Object.keys(studentCountsByField)) {
+      const studentsInField = lines
         .filter((line) => line.includes(field))
         .map((line) => line.split(',')[0].trim())
-        .join(', ')}`);
+        .join(', ');
+
+      console.log(`Number of students in ${field}: ${studentCountsByField[field]}. List: ${studentsInField}`);
     }
   } catch (error) {
     throw new Error('Cannot load the database');
   }
 }
-
-// Llama a la función con la ruta al archivo CSV
-countStudents('database.csv');
-
 
 module.exports = countStudents;
