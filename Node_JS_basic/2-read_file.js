77f94f865960c088cf8a581df452e85893b534ab
Jose Suarez <jolusuvi@gmail.com>
2023-11-05 23:19:46 -0500

    To read a file synchronously

---
+++ Node_JS_basic/2-read_file.js
@@ -0,0 +1,42 @@
+const fs = require('fs');
+const parse = require('csv-parse/lib/sync');
+
+function countStudents(path) {
+  
+  try {
+    const csvData = fs.readFileSync(path, 'utf8');
+    // Does not take into consideration the first line
+    const records = parse(csvData, {
+      columns: true,
+      skip_empty_lines: true,
+    });
+
+    const studentCountsByField = {};
+
+    for (const record of records) {
+      const field = record.field;
+
+      // Logs the number of students in each field
+      if (studentCountsByField[field]) {
+        studentCountsByField[field]++;
+      } else {
+        studentCountsByField[field] = 1;
+      }
+    }
+
+    const totalStudents = records.length;
+
+    console.log(`Number of students: ${totalStudents}`);
+
+    for (const field in studentCountsByField) {
+      console.log(`Number of students in ${field}: ${studentCountsByField[field]}. List: ${records
+        .filter((record) => record.field === field)
+        .map((record) => record.firstname)
+        .join(', ')}`);
+    }
+  } catch (error) {
+    throw new Error('Cannot load the database');
+  }
+}
+
+module.exports = displayMessage;
