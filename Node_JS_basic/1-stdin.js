e72c009d2591eb92a71f148d839db4626ba7a602
Jose Suarez <jolusuvi@gmail.com>
2023-11-05 23:01:06 -0500

    To pass lint

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -5,7 +5,7 @@ process.stdout.write('Welcome to Holberton School, what is your name?\n');
 process.stdin.on('readable', () => {
   const name = process.stdin.read();
 
-  if (name !== null){
+  if (name !== null) {
     process.stdout.write(`Your name is: ${name}`);
   }
 });
