27b19a7e39a5dabd54ba6b7435a2cccfa68e13a3
Jose Suarez <jolusuvi@gmail.com>
2023-11-22 14:53:19 -0500

    To print the title of a Star Wars movie

---
+++ javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const request = require('request');
+const starWarsFilms = 'https://swapi-api.hbtn.io/api/films/'.concat(process.argv[2]);
+
+request(starWarsFilms, (error, response, body) => {
+  if (error) console.log(error);
+  if (response === 200) {
+  filmData = JSON.parse(body); //converts it into a JavaScript object
+  console.log(filmData.title);
+  };
+});
+
