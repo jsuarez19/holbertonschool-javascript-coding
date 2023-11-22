16e1a14f2e01524f3e8f366e79f5a518e00456a3
Jose Suarez <jolusuvi@gmail.com>
2023-11-22 17:01:23 -0500

    To handle errors

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -6,16 +6,23 @@ const id = 'https://swapi-api.hbtn.io/api/people/18/';
 let count = 0;
 
 request(starWarsFilmsList, (error, response, body) => {
-  if (error) console.log(error);
+  if (error) {
+    console.error(error);
+    return;
+  }
   if (response.statusCode === 200) {
-    const filmsData = JSON.parse(body); // converts it into a JavaScript object
-    for (const film of filmsData.results) {
-      for (const character of film.characters) {
-        if (character.includes(id)) {
-          count = count + 1;
+    try{
+      const filmsData = JSON.parse(body); // converts it into a JavaScript object
+      for (const film of filmsData.results) {
+        for (const character of film.characters) {
+          if (character.includes(id)) {
+            count = count + 1;
+          }
         }
       }
+      console.log(count);
+    } catch (parseError) {
+      console.error('Error parsing JSON')
     }
-    console.log(count);
   }
 });
