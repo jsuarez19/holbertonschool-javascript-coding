c39ea272957be5910b98aff9fb01fef6c469951f
Jose Suarez <jolusuvi@gmail.com>
2023-11-22 14:38:17 -0500

    Semistandard validation

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -4,5 +4,5 @@ const request = require('request');
 
 request(process.argv[2], (error, response) => {
   if (error) console.log(error);
-  console.log(`code: ${response.statusCode}`)
+  console.log(`code: ${response.statusCode}`);
 });
