2e9e8c502a4193eb2c726229d85f13ac611152aa
Jose Suarez <jolusuvi@gmail.com>
2023-11-22 20:07:04 -0500

    Semistandard validation

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -3,17 +3,16 @@
 const request = require('request');
 const fs = require('fs');
 
-
 request(process.argv[2], (error, response, body) => {
   if (error) {
     console.error(error);
     return;
   }
-  
+
   if (response.statusCode === 200) {
-    data = JSON.parse(body);
+    const data = JSON.parse(body);
     fs.writeFile(process.argv[3], data, 'utf-8', (err) => {
       if (err) console.log(err);
     });
-  }
+  };
 })
