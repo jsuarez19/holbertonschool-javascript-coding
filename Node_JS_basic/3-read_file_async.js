a6ed1d1931d9c2945e3e149e9852331ade20f9c9
Jose Suarez <jolusuvi@gmail.com>
2023-11-23 16:21:55 -0500

    To pass lint

--- Node_JS_basic/3-read_file_async.js
+++ Node_JS_basic/3-read_file_async.js
@@ -1,11 +1,11 @@
 const fs = require('fs');
 
 function countStudents(path) {
-  return new Promise(async (resolve, reject) => {
+  return new Promise((resolve, reject) => {
     try {
       const data = await fs.readFile(path, 'utf8');
       const lines = data.trim().split('\n').slice(1);
-      
+
       // Initialize the count for both fields
       const studentCountsByField = { CS: 0, SWE: 0 };
       const csStudents = [];
@@ -31,9 +31,9 @@ function countStudents(path) {
       console.log(`Number of students in SWE: ${studentCountsByField.SWE}. List: ${sweStudents.join(', ')}`);
       resolve();
     } catch (error) {
-        reject('Cannot load the database');
+      reject(throw new Error('Cannot load the database'));
     }
-  })
+  });
 }
 
 module.exports = countStudents;
