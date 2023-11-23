8c39c43f2a927bdfda76ba5454cf88a540007f9a
Jose Suarez <jolusuvi@gmail.com>
2023-11-23 02:33:34 -0500

    To redo the function

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -4,34 +4,33 @@ function countStudents(path) {
   try {
     const csvData = readFileSync(path, 'utf8');
     const lines = csvData.split('\n');
+    // Initialize the count for both fields
+    const studentCountsByField = { CS: 0, SWE: 0 };
+    const csStudents = [];
+    const sweStudents = [];
 
-    const studentCountsByField = {};
     let totalStudents = 0;
 
     for (const line of lines) {
       const columns = line.split(',');
-      const [, , , field] = columns.map((column) => column.trim());
+      const [name, , , field] = columns.map((column) => column.trim());
 
-      if (studentCountsByField[field]) {
-        studentCountsByField[field] += 1;
-      } else {
-        studentCountsByField[field] = 1;
+      if (field === 'CS') {
+        studentsCountsByField.CS += 1;
+        csStudents.push(name.trim());
+      } else if (field === 'SWE') {
+        studentsCountsByField.SWE += 1;
+        sweStudents.push(name.trim());
       }
 
       totalStudents += 1;
     }
 
-    console.log(`Number of students: ${totalStudents}`);
-
-    for (const field of Object.keys(studentCountsByField)) {
-      const studentsInField = lines
-        .filter((line) => line.includes(field))
-        .map((line) => line.split(',')[0].trim())
-        .join(', ');
-
-      console.log(`Number of students in ${field}: ${studentCountsByField[field]}. List: ${studentsInField}`);
-    }
-  } catch (error) {
+    console.log(`Number of students: ${totalStudents}`)
+    console.log(`Number of students in CS: ${studentsCountsByField.CS}. List: ${csStudents.join(', ')}`);
+    console.log(`Number of students in SWE: ${studentsCountsByField.SWE}. List: ${sweStudents.join(', ')}`);
+  }
+  catch (error) {
     throw new Error('Cannot load the database');
   }
 }
