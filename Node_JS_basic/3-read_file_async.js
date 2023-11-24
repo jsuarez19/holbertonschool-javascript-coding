e1695b44753b5216c20b37d86d4aa89e18f87069
Jose Suarez <jolusuvi@gmail.com>
2023-11-23 21:16:35 -0500

    To use .then

--- Node_JS_basic/3-read_file_async.js
+++ Node_JS_basic/3-read_file_async.js
@@ -1,39 +1,38 @@
 const fs = require('fs').promises;
 
 function countStudents(path) {
-  return new Promise((resolve, reject) => {
-    try {
-      const data = fs.readFile(path, 'utf8');
-      const lines = data.trim().split('\n').slice(1);
+  return fs.readFile(path, 'utf8')
+      .then((data) => {
+        const data = fs.readFile(path, 'utf8');
+        const lines = data.trim().split('\n').slice(1);
 
-      // Initialize the count for both fields
-      const studentCountsByField = { CS: 0, SWE: 0 };
-      const csStudents = [];
-      const sweStudents = [];
-      let totalStudents = 0;
+        // Initialize the count for both fields
+        const studentCountsByField = { CS: 0, SWE: 0 };
+        const csStudents = [];
+        const sweStudents = [];
+        let totalStudents = 0;
 
-      lines.forEach((line) => {
-        const [firstname, , , field] = line.split(',');
+        lines.forEach((line) => {
+          const [firstname, , , field] = line.split(',');
 
-        if (field === 'CS') {
-          studentCountsByField.CS += 1;
-          csStudents.push(firstname.trim());
-        } else if (field === 'SWE') {
-          studentCountsByField.SWE += 1;
-          sweStudents.push(firstname.trim());
-        }
+          if (field === 'CS') {
+            studentCountsByField.CS += 1;
+            csStudents.push(firstname.trim());
+          } else if (field === 'SWE') {
+            studentCountsByField.SWE += 1;
+            sweStudents.push(firstname.trim());
+          }
 
-        totalStudents += 1;
-      });
+          totalStudents += 1;
+        });
 
-      console.log(`Number of students: ${totalStudents}`);
-      console.log(`Number of students in CS: ${studentCountsByField.CS}. List: ${csStudents.join(', ')}`);
-      console.log(`Number of students in SWE: ${studentCountsByField.SWE}. List: ${sweStudents.join(', ')}`);
-      resolve();
-    } catch (error) {
-      reject(new Error('Cannot load the database'));
-    }
-  });
+        console.log(`Number of students: ${totalStudents}`);
+        console.log(`Number of students in CS: ${studentCountsByField.CS}. List: ${csStudents.join(', ')}`);
+        console.log(`Number of students in SWE: ${studentCountsByField.SWE}. List: ${sweStudents.join(', ')}`);
+        resolve();
+      })
+      .then(() => Promise.resolve())
+      .catch((error) => Promise.reject(new Error('Cannot load the database')));
 }
 
 module.exports = countStudents;
