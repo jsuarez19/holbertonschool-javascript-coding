08a0a1d93f52d9b4ede6aa0abbc9fae9378b4f83
Jose Suarez <jolusuvi@gmail.com>
2023-11-25 21:37:04 -0500

    To pass lint

--- Node_JS_basic/6-http_express.js
+++ Node_JS_basic/6-http_express.js
@@ -1,9 +1,9 @@
-const express = require('express')
+const express = require('express');
 
 const app = express();
 const PORT = 1245;
 
-app.get('/', (req, res) => res.send('Hello Holberton School!'))
+app.get('/', (req, res) => res.send('Hello Holberton School!'));
 app.listen(PORT);
 
 module.exports = app;
